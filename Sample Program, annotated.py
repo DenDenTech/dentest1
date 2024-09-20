2afdc6fea6fd5b8f357bb7087e06eae2911b656c
Den <164683086+DenDenTech@users.noreply.github.com>
2024-09-20 15:08:41 -0500

    Add files via upload

---
+++ Sample Program, annotated.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 15
+print("Literal:", x)
+
+# Integer
+y = -25
+print("Integer:", y)
+
+# Float
+z = 50.5
+print("Float:", z)
+
+# String
+name = "Dennis"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
