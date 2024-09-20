609fac2935d3d723ce911d91ee114570ef1358e2
Den <164683086+DenDenTech@users.noreply.github.com>
2024-09-20 13:44:58 -0500

    Add files via upload

---
+++ knock-knock-joke-assignment.py
@@ -0,0 +1,11 @@
+# Knock-Knock Joke Program
+# This program tells a knock-knock joke using the name "Bob"
+
+print("Knock, knock.")
+print("Who's there?")
+print("Bob.")
+print("Bob who?")
+print("Bob your head if you love Python!")
+
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nBob.\nBob who?\nBob your head if you love Python!")
