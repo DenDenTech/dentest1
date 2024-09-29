dc77c87db923f40a50b156435f8bc42be5f1d410
Den <164683086+DenDenTech@users.noreply.github.com>
2024-09-29 11:54:01 -0500

    Add files via upload

---
+++ adding comments assignment.py
@@ -0,0 +1,13 @@
+# This program calculates the miles per gallon (mpg) for a vehicle
+
+# Declare the size of the fuel tank in gallons
+tank_size = 15
+
+# Declare the distance traveled in miles
+distance = 600
+
+# Calculate the miles per gallon (mpg)
+mpg = distance / tank_size
+
+# Display the result
+print("Miles per gallon:", mpg)
