8c342e76abd1f4d400fb1d9160f5c750d71ba1c0
Den <164683086+DenDenTech@users.noreply.github.com>
2024-09-29 11:28:29 -0500

    Add files via upload

---
+++ area of rectangle assigment.py
@@ -0,0 +1,11 @@
+# This program calculates the area of a rectangle and displays the result
+
+# Declare variables for length and width
+length = 7  
+width = 3  
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Display the result with appropriate formatting
+print("\nThe area of the rectangle with a length of {} units and a width of {} units is {} square units.".format(length, width, area))
