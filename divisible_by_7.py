08a5f3a936178011cec38597423e22e2674dd15a
Den <164683086+DenDenTech@users.noreply.github.com>
2024-10-02 12:39:58 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,6 @@
+# Iterate through numbers from 1 to 300
+for number in range(1, 301):
+    # Check if the number is divisible by 7
+    if number % 7 == 0:
+        # Print the number if it is divisible by 7
+        print(number)
